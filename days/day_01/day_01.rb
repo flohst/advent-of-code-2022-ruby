b546d01c4787c7d2cd430dcd42c6dd1b5b88d72e
Florian Stöhr <florian@x-flexgolf.de>
2022-12-01 20:50:38 +0100

    refactor: Clean up the code for day 1

--- days/day_01/day_01.rb
+++ days/day_01/day_01.rb
@@ -1,9 +1,7 @@
-input = File.read("day_01_input.txt").split("\n\n").map {|elf| elf.split("\n")}
-input_as_int = input.map {|elf| elf.map {|item| item.to_i()}}
-total_calories = input_as_int.map {|elf| elf.reduce(:+)}
+input_as_int = File.read("day_01_input.txt").split("\n\n").map {|elf| elf.split("\n").map {|item| item.to_i()}}
+total_calories = input_as_int.map {|elf| elf.sum()}
 max_calories = total_calories.max()
 puts max_calories
 
-max_three_calories = total_calories.max(3)
-top_three_calories = max_three_calories.sum()
+top_three_calories = total_calories.max(3).sum()
 puts top_three_calories
